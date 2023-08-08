379268de25f0e79b58566dfb03434aa8f9cb76ca
bx <2322750326@qq.com>
2023-08-08 11:52:56 +0800

    菜品使用redis缓存优化

--- src/main/java/com/bx/reggie/controller/DishController.java
+++ src/main/java/com/bx/reggie/controller/DishController.java
@@ -13,9 +13,11 @@ import com.bx.reggie.utils.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -26,7 +28,7 @@ import java.util.stream.Collectors;
 @RestController
 @Slf4j
 @RequestMapping("/dish")
-
+@SuppressWarnings({"all"})
 public class DishController {
 	@Autowired
 	private DishService dishService;
@@ -34,13 +36,17 @@ public class DishController {
 	private CategoryService categoryService;
 	@Autowired
 	private DishFlavourService dishFlavourService;
-//	@Autowired
+    @Autowired
+	private RedisTemplate redisTemplate;
 	
 	
 	//添加菜品
 	@PostMapping
 	public R<String> save(@RequestBody DishDto dishDto) {
+		String key="Dish_"+dishDto.getCategoryId()+"_1";
 		dishService.saveWithFlavour(dishDto);
+		redisTemplate.delete(key);
+		
 		return R.success("添加菜品成功");
 	}
 	
@@ -58,7 +64,7 @@ public class DishController {
 		//添加条件
 		pageLambdaQueryWrapper.like(name != null, Dish::getName, name);
 		//添加排序条件
-		pageLambdaQueryWrapper.orderByAsc(Dish::getUpdateTime);
+		pageLambdaQueryWrapper.orderByDesc(Dish::getUpdateTime);
 		//执行查询
 		dishService.page(pageInfo, pageLambdaQueryWrapper);
 		
@@ -97,14 +103,43 @@ public class DishController {
 	//修改菜品
 	@PutMapping
 	public R<String> update(@RequestBody DishDto dishDto) {
+		String key="Dish_"+dishDto.getCategoryId()+"_"+dishDto.getStatus();
 		dishService.updateWithFlavour(dishDto);
+		redisTemplate.delete(key);
 		return R.success("修改成功");
 	}
+	
+
+	//删除菜品
+	@DeleteMapping()
+	public R<String> delete( Long ids){
+		Dish dish = dishService.getById(ids);
+		String key="Dish_"+dish.getCategoryId()+"_"+dish.getStatus();
+		dishService.removeById(ids);
+		redisTemplate.delete(key);
+		return R.success("删除菜品成功");
+	}
+	
 	
 	
 	//菜品展示到用户端点菜
 	@GetMapping("/list")
 	public R<List<DishDto>> list(Dish dish) {
+		List<DishDto> dishDtoList=null;
+		//构造key，根据菜系id和在售状态
+		String key = "Dish_"+dish.getCategoryId()+"_"+dish.getStatus();
+		log.info("========key为====================================================={}",key);
+		//返回该key在redis缓存中的菜品
+		 dishDtoList =(List<DishDto>) redisTemplate.opsForValue().get(key);
+		 
+		 
+		//如果不为空，直接返回给前端
+		 if(dishDtoList!=null){
+			 log.info("-----------缓存中dishDtoList为-------------------------------------{}",dishDtoList);
+			 return  R.success(dishDtoList);
+		 }
+		 //为空则再去数据库中查
+		
 		//构造条件过滤器
 		LambdaQueryWrapper<Dish> dishLambdaQueryWrapper = new LambdaQueryWrapper<>();
 		//添加条件，菜品id
@@ -112,11 +147,11 @@ public class DishController {
 		//菜品在售卖
 		dishLambdaQueryWrapper.eq(Dish::getStatus, 1);
 		//添加排序
-		dishLambdaQueryWrapper.orderByAsc(Dish::getSort).orderByAsc(Dish::getUpdateTime);
+		dishLambdaQueryWrapper.orderByDesc(Dish::getSort).orderByAsc(Dish::getUpdateTime);
 		//执行回显每一个菜品
 		List<Dish> list = dishService.list(dishLambdaQueryWrapper);
 		//用户端点菜时需展示菜品口味
-		List<DishDto> dishDtoList = list.stream().map((res) -> {
+		dishDtoList = list.stream().map((res) -> {
 					DishDto dishDto = new DishDto();
 					//将菜品中回显的每个菜品单独拷贝到dishDto里
 					BeanUtils.copyProperties(res, dishDto);
@@ -142,6 +177,10 @@ public class DishController {
 					return dishDto;
 				}
 		).collect(Collectors.toList());//将dishDto里的添加上菜系和口味的每个菜品封装成集合返回
+		
+		log.info("-----------数据库中dishDtoList为-----------------------------------------{}",dishDtoList);
+		//将查到的菜品添加到redis缓存中
+		redisTemplate.opsForValue().set(key,dishDtoList,60, TimeUnit.MINUTES);
 		
 		//将添加了菜系和口味的菜品集合返回给前端
 		return R.success(dishDtoList);
--- src/main/java/com/bx/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/bx/reggie/filter/LoginCheckFilter.java
@@ -24,7 +24,8 @@ import java.io.IOException;
 
 public class LoginCheckFilter implements Filter {
 	//用来匹配路径是否属于大路径中包含的
-	private  static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+	private static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+	
 	@Override
 	/**
 	 * 登录拦截器思想(这里使用web中的过滤器)
@@ -40,31 +41,32 @@ public class LoginCheckFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
 		//获取本次请求的路径
 		String requestURI = request.getRequestURI();
-		log.info("拦截到的请求：{}",requestURI);
+		if (!requestURI.equals("/common/download")) {
+			log.info("{}", requestURI);
+		}
 		//定义这些请求不需要要拦截
-		String[] urls=new String[]{
-                "/employee/login",
-                "/employee/logout",
-                "/backend/**",
-                "/front/**",
+		String[] urls = new String[]{
+				"/employee/login",
+				"/employee/logout",
+				"/backend/**",
+				"/front/**",
 				"/user/sendMsg",
 				"/user/login"
-        };
+		};
 		
 		//判断是否需要拦截该请求
 		boolean check = check(urls, requestURI);
 		
 		//不需要直接放行
-		if(check){
-			log.info("本次请求{}不需要拦截",requestURI);
+		if (check) {
+			log.info("本次请求{}不需要拦截", requestURI);
 			//放行
 			filterChain.doFilter(request, response);
 			return;
 		}
 		
 		//需要拦截，判断是否登录，已经登录直接放行
-		if (request.getSession().getAttribute("employee")!=null){
-			log.info("用户已登录，用户ID为:{}",request.getSession().getAttribute("employee"));
+		if (request.getSession().getAttribute("employee") != null) {
 			Long emp = (Long) request.getSession().getAttribute("employee");
 			BaseContext.set(emp);
 			filterChain.doFilter(request, response);
@@ -72,8 +74,8 @@ public class LoginCheckFilter implements Filter {
 		}
 		
 		
-		if (request.getSession().getAttribute("user")!=null){
-			log.info("用户已登录，用户ID为:{}",request.getSession().getAttribute("user"));
+		if (request.getSession().getAttribute("user") != null) {
+//			log.info("用户已登录，用户ID为:{}",request.getSession().getAttribute("user"));
 			Long userId = (Long) request.getSession().getAttribute("user");
 			BaseContext.set(userId);
 			filterChain.doFilter(request, response);
@@ -87,10 +89,10 @@ public class LoginCheckFilter implements Filter {
 	
 	
 	//此次请求是否属于不需要要拦截的请求
-	public boolean check(String[] urls,String requestURI){
-		for (String url :urls) {
-			boolean match = PATH_MATCHER.match(url,requestURI);
-			if (match){
+	public boolean check(String[] urls, String requestURI) {
+		for (String url : urls) {
+			boolean match = PATH_MATCHER.match(url, requestURI);
+			if (match) {
 				return true;
 			}
 		}
